e57e1a96c3cf08f01d726cdbafb1eaeca60b1121
Markosaur <mark3pace@gmail.com>
2019-09-28 15:19:05 -0600

    completed

---
+++ server/middlewares/checkForSession.js
@@ -0,0 +1,21 @@
+// module.exports = function(req, res, next){
+//     const { session } = req;
+//     if ( !session.user ) {
+//         session.user = {
+//             username: '', 
+//             cart: [], 
+//             total: 0
+//         };
+//     }
+//     next();
+// };
+
+module.exports = function(req, res, next) {
+    const { session } = req;
+  
+    if (!session.user) {
+      session.user = { username: "", cart: [], total: 0 };
+    }
+  
+    next();
+  };
